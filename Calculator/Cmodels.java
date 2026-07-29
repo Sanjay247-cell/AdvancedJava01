859b28b2fafbb27db961c85e6ecf0b6e9954e2c6
Sanjay Yadav <yadavsk6161@gmail.com>
2026-07-29 14:52:22 +0530

    Add files via upload

---
+++ Calculator/Cmodels.java
@@ -0,0 +1,25 @@
+
+
+public class Cmodels {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            return 0;
+        }
+        return a / b;
+    }
+    public double percentage (double a, double b)
+{
+    return ( a + b) / 100;
+}}
---
+++ Calculator/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+void main(){
+    Cviews cv = new Cviews();
+    Cmodels cm = new Cmodels();
+    new Ccontroller (cm, cv);
+    System.out.print("Calculator");
+}
+}
